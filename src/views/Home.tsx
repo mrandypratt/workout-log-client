713c15b6f97cc6b7ad08817c72e9e40b7225dd50
mrandypratt <mrandypratt@gmail.com>
2022-04-05 15:25:56 -0500

    Homepage finished

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -3,18 +3,30 @@
 export const Home = () => {
 
   return (
-    <div style={{textAlign: "center", fontStyle: "Roboto"}}>
+    <div className="homepage">
 
-      <div style={{float: "left", textAlign: "left", marginLeft: "10%", marginTop: "10%"}}>
-        <h1 style={{fontSize: "1.5em"}}>Hello!</h1>
-        <p>My name is <b className="orange-pop">Andy Pratt</b> and</p>
-        <p>I am a <b className="blue-pop">Full-Stack Developer</b>.</p>
+      <div className="landing">
+        
+        <div className="split left">
+
+          <div className="centered greeting">
+            <h1>Hello! &#128075;</h1>
+            <p>My name is <b className="orange-pop">Andy Pratt</b> and</p>
+            <p>I am a <b className="blue-pop">Full-Stack Developer</b>.</p>
+          </div>
+
+        </div>
+
+        <div className="split right">
+
+          <div className="centered profile-pic">
+            <img src={require("../Profile.jpeg")} alt="<APDev/>"></img>  
+          </div>
+
+        </div>
+
       </div>
 
-
-      <div style={{float: "right", maxWidth: "50%"}}>
-        <img src={require("../Profile.jpeg")} alt="<APDev/>" style={{maxWidth: "75%", height: "auto"}}></img>  
-      </div>
     </div>
   );
 }
