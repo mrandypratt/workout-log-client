f646e48f1967fef4d7fe4c5993a7d3c59efd2232
mrandypratt <mrandypratt@gmail.com>
2022-11-08 23:48:02 -0600

    Set up root React Router logic

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,22 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import "./styles/App.css"
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+]);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
   document.getElementById('root')
 );
