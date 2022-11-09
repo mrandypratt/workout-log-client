95bef02e9c9f053d5b4c62cad753ddc7063b35cd
mrandypratt <mrandypratt@gmail.com>
2022-11-08 23:47:10 -0600

    Exported app level styles to App.css

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,11 @@ import { VIEWS } from "./constants/views";
 import { Navbar } from "./components/Navbar"
 import { Home } from "./views/Home";
 import { Portfolio } from "./views/Portfolio"
-import { Resume } from "./views/Resume";
 import { Footer } from "./components/Footer";
-import { useEffect } from "react";
+
 
 export const App = (): JSX.Element => {
   const [view, setView] = useState(VIEWS.home); 
-
-  useEffect(() => {
-    document.body.style.backgroundColor = "#292929";
-    document.body.style.fontStyle = "Roboto";
-    document.body.style.color = "white";
-    
-    return () => {
-      document.body.style.backgroundColor = "white";
-    };
-  });
 
 
   return (
