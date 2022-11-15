27a52fa97c3b59a4d1d46f92c1ac8901f6ce7c07
mrandypratt <mrandypratt@gmail.com>
2022-11-14 20:55:52 -0600

    Updated Home Page with message and no picture

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -4,7 +4,10 @@ export const Home = () => {
 
   return (
     <div className="home page-container">
-      <h1 className="home-greeting">Home Page</h1>
+      <h1 className="home-greeting">Hello! My name is </h1>
+      <h2 className="orange-pop">Andy Pratt.</h2>
+
+      <p className="home-paragraph">I'm a product-oriented <b className="blue-pop">Software Engineer</b> who loves building products and automating processes.</p>
         {/* <h1>Hello! &#128075;</h1> */}
         {/* <p>My name is <b className="orange-pop">Andy Pratt</b> and I am a <b className="blue-pop">Full-Stack Developer</b>.</p> */}
 
