18443ba4ba91a39be9767517f4dc9abc52f0bfc3
mrandypratt <mrandypratt@gmail.com>
2022-11-24 02:14:40 -0600

    Added Responsive layout to portfolio page

--- src/views/Portfolio.tsx
+++ src/views/Portfolio.tsx
@@ -18,31 +18,43 @@ export const Portfolio = () => {
 
         <div className="portfolio-summary-container">
           <p className="portfolio-summary">
-            Cards with Friends is an online multi-player Cards Against Humanity clone that can be played online in any modern web browser.
+            Cards with Friends is a multi-player Cards Against Humanity web app.
+          </p>
+          <p className="portfolio-summary">
+            It also happens to be my first solo product to design, build, and deploy!
           </p>
         </div>
 
         <div className="portfolio-cta-container">
           <h2 className="portfolio-cta">
-            Please check out my Cards with Friends journey below!
+            Please check out my journey below!
           </h2>
         </div>
 
         <div className="portfolio-project-card-container" id="project1">
-          <h3 className="portfolio-project-card">
-            V1: Single-Device (MVP)
+          <h3 className="portfolio-project-card-header">
+            Version 1:
+          </h3>
+          <h3 className="portfolio-project-card-header">
+            Single-Device Gameplay (MVP)
           </h3>
         </div>
 
         <div className="portfolio-project-card-container" id="project2">
-          <h3 className="portfolio-project-card">
-            V2: Multi-Device
+          <h3 className="portfolio-project-card-header">
+            Version 2:
+          </h3>
+          <h3 className="portfolio-project-card-header">
+            Multi-Device Gameplay
           </h3>
         </div>
 
         <div className="portfolio-project-card-container" id="project3">
-          <h3 className="portfolio-project-card">
-            V3: Single-Player
+          <h3 className="portfolio-project-card-header">
+            Version 3
+          </h3>
+          <h3 className="portfolio-project-card-header">
+            Single-Player: Online Simulation
           </h3>
         </div>
       </div>
